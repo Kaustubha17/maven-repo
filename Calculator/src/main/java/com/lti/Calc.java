7eb4d450d8c7a3f9c69ee6b7313cdd586503321c
Kaustubha17 <kaustubhashah17@gmail.com>
2020-12-21 15:16:25 +0530

    Div method added

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -14,12 +14,15 @@ public class Calc {
 	public static int prod(int num1,int num2) {
 		return num1*num2;
 	}
+	public static int div(int num1,int num2) {
+		return num1/num2;
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("Sum= "+add(2,3));
 		System.out.println("diff= "+diff(23,23));
 		System.out.println("prod= "+prod(23,23));
-		
+		System.out.println("div= "+div(23,23));
 		
 	}
 

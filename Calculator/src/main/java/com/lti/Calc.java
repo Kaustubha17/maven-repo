2f1686f937c772b6ce04acb082e91964589ae60a
Kaustubha17 <kaustubhashah17@gmail.com>
2020-12-21 15:10:21 +0530

    prod method added

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -1,5 +1,7 @@
 package com.lti;
 
+
+
 public class Calc {
 
 	public static int add(int num1,int num2) {
@@ -8,10 +10,17 @@ public class Calc {
 	public static int diff(int num1,int num2) {
 		return num1-num2;
 	}
+	
+	public static int prod(int num1,int num2) {
+		return num1*num2;
+	}
 	public static void main(String[] args) {
 		
-		System.out.println("Sum= "+add(23,23));
+		System.out.println("Sum= "+add(2,3));
 		System.out.println("diff= "+diff(23,23));
+		System.out.println("prod= "+prod(23,23));
+		
+		
 	}
 
 }

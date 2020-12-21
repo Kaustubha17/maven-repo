a7face085b7d8f1d50f4f486c2da924a3f6ae89d
Kaustubha17 <kaustubhashah17@gmail.com>
2020-12-21 14:58:30 +0530

    Diff and desc. added

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -5,10 +5,13 @@ public class Calc {
 	public static int add(int num1,int num2) {
 		return num1+num2;
 	}
-	
+	public static int diff(int num1,int num2) {
+		return num1-num2;
+	}
 	public static void main(String[] args) {
 		
-		System.out.println(add(23,23));
+		System.out.println("Sum= "+add(23,23));
+		System.out.println("diff= "+diff(23,23));
 	}
 
 }
